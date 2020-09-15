529a7325be59a6fa6f18f9af56c887d2752403cc
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-15 23:53:54 +0300

    Character class implementation

---
+++ src/model/character.cpp
@@ -0,0 +1,23 @@
+#include "character.h"
+
+Character::Character(const std::string &name, const std::string &biography) {
+    Character::name = name;
+    Character::biography = biography;
+}
+
+const std::string &Character::get_name() const {
+    return Character::name;
+}
+
+const std::string &Character::get_biography() const {
+    return Character::biography;
+}
+
+bool Character::operator==(const Character &rhs) const {
+    return name == rhs.name &&
+           biography == rhs.biography;
+}
+
+bool Character::operator!=(const Character &rhs) const {
+    return !(rhs == *this);
+}
