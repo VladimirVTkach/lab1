fa0236e8543efa2968e86e19dc262d308527f93c
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-15 19:27:08 +0300

    all tests run from main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,6 @@
-#include <iostream>
-#include <string>
-#include <chrono>
-#include "book.h"
+#include "../tst/tests_runner.h"
 
 int main() {
-    Author author("Victor",
-                  "Pelevin",
-                  std::chrono::year_month_day(std::chrono::year(2020),
-                                              std::chrono::month(9),
-                                              std::chrono::day(15)));
-    std::vector<Author> authors;
-    authors.push_back(author);
-
-    Book book("Generation P",
-              authors,
-              322,
-              std::chrono::year_month_day(std::chrono::year(2020),
-                                          std::chrono::month(9),
-                                          std::chrono::day(15)),
-              "description");
-
-    std::cout << book.get_name() << std::endl;
+    run_all_tests();
     return 0;
 }
