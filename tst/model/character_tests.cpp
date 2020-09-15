66e6bf3cd90ecdc481baeb8004e89ee9c36d655f
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-15 23:58:31 +0300

    Character class tests

---
+++ tst/model/character_tests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "../../src/model/character.h"
+
+TEST(character_tests, test_get_name) {
+    std::string name = "Woland";
+    Character character(name,
+                        "");
+    ASSERT_EQ(name, character.get_name());
+}
+
+
+TEST(character_tests, test_get_biography) {
+    std::string biography = "Evil character";
+    Character character("",
+                        biography);
+    ASSERT_EQ(biography, character.get_biography());
+}
