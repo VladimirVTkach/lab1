de4731d23ef4b36d1c514deb7b855c14c8a1e99d
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-15 17:50:54 +0300

    missed cosnstructor impl added

--- book.cpp
+++ book.cpp
@@ -1,5 +1,17 @@
 #include "book.h"
 
+Book::Book(const std::string &name,
+     const std::vector<Author> &authors,
+     int pages,
+     const std::chrono::year_month_day &release_date,
+     const std::string &description) {
+    Book::name = name;
+    Book::authors = authors;
+    Book::pages = pages;
+    Book::release_date = release_date;
+    Book::description = description;
+}
+
 const std::string &Book::getName() const {
     return name;
 }
