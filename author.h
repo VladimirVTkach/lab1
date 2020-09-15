e4e54778fd52441f9d1676cd62f77ee8f8f3e678
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-15 16:40:13 +0300

    constructor added

--- author.h
+++ author.h
@@ -3,6 +3,10 @@
 
 class Author {
 public:
+    Author(std::string first_name,
+           std::string last_name,
+           std::chrono::year_month_day date_of_birth);
+
     std::string get_first_name();
 
     std::string get_last_name();
