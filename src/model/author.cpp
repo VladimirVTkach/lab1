e99a28b3ea81668f27ff6b497c105fb225d45da0
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-27 12:58:28 +0300

    initialization of member variables inside constructor's body replaced by initializer list

--- src/model/author.cpp
+++ src/model/author.cpp
@@ -2,11 +2,10 @@
 
 Author::Author(const std::string &first_name,
                const std::string &last_name,
-               const std::chrono::year_month_day &date_of_birth) {
-    Author::first_name = first_name;
-    Author::last_name = last_name;
-    Author::date_of_birth = date_of_birth;
-}
+               const std::chrono::year_month_day &date_of_birth) :
+        first_name(first_name),
+        last_name(last_name),
+        date_of_birth(date_of_birth) {}
 
 const std::string &Author::get_first_name() const {
     return Author::first_name;
