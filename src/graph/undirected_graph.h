7f931d0b13e7d7e7a664fe9c67662f5bf813db39
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-28 16:50:25 +0300

    UndirectedGraph abstract class declared

---
+++ src/graph/undirected_graph.h
@@ -0,0 +1,27 @@
+#pragma once
+
+template<typename T>
+struct Vertex {
+    T data;
+};
+
+template<typename T>
+struct Edge {
+    Vertex<T> lhs;
+    Vertex<T> rhs;
+};
+
+template<typename T>
+class UndirectedGraph {
+    virtual void AddVertex(const Vertex<T> &vertex) = 0;
+
+    virtual void AddEdge(const Edge<T> &edge) = 0;
+
+    virtual void RemoveVertex(const Vertex<T> &vertex) = 0;
+
+    virtual void RemoveEdge(const Edge<T> &edge) = 0;
+
+    virtual bool IsLinked() = 0;
+
+    virtual int GetDistance(const Vertex<T> &src, const Vertex<T> &dst) = 0;
+};
