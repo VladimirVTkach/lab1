227d64c2989f7601b345b179e4b5fcb6f543f512
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-28 17:48:49 +0300

    GetAdjacentEdgesOrEmptyList renamed to GetAdjacentEdgesOrEmptySet and now returns set

--- src/graph/adjacent_undirected_graph.h
+++ src/graph/adjacent_undirected_graph.h
@@ -12,7 +12,11 @@ public:
     }
 
     void AddEdge(const Edge<T> &edge) override {
+        Vertex<T> &lhs = edge.lhs;
+        Vertex<T> &rhs = edge.rhs;
 
+        auto lhs_adjacent_edges = GetAdjacentEdgesOrEmptyList(lhs);
+        auto rhs_adjacent_edges = GetAdjacentEdgesOrEmptyList(lhs);
     }
 
     void RemoveVertex(const Vertex<T> &vertex) override {
@@ -31,10 +35,10 @@ public:
         return 0;
     }
 
-    std::vector<Edge<Vertex<T>>> GetAdjacentEdgesOrEmptyList(Vertex<T> &vertex) override {
+    std::set<Edge<Vertex<T>>> GetAdjacentEdgesOrEmptySet(Vertex<T> &vertex) override {
         const auto &found_vertex_it = FindVertexIt(vertex);
         if (found_vertex_it == adjacency_lists_.end()) {
-            return std::vector<Edge<Vertex<T>>>();
+            return std::set<Edge<Vertex<T>>>();
         } else {
             return found_vertex_it->second;
         }
