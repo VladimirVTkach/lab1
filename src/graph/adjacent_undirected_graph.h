a0e3ac82232c7c922815c0828ad1a1c04c49fb2d
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-28 17:21:43 +0300

    AdjacentUndirectedGraph implementation of UndirectedGraph is created and GetVerticesCount is implemented

---
+++ src/graph/adjacent_undirected_graph.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include "undirected_graph.h"
+
+template<typename T>
+class AdjacentUndirectedGraph : public UndirectedGraph<T> {
+public:
+    void AddVertex(const Vertex<T> &vertex) override {
+        adjacency_lists_.try_emplace(vertex, std::vector<Edge<T>>());
+    }
+
+    void AddEdge(const Edge<T> &edge) override {
+
+    }
+
+    void RemoveVertex(const Vertex<T> &vertex) override {
+
+    }
+
+    void RemoveEdge(const Edge<T> &edge) override {
+
+    }
+
+    bool IsLinked() override {
+        return false;
+    }
+
+    int GetDistance(const Vertex<T> &src, const Vertex<T> &dst) override {
+        return 0;
+    }
+
+    size_t GetVerticesCount() override {
+        return adjacency_lists_.size();
+    }
+
+    size_t GetEdgesCount() override {
+        return 0;
+    }
+
+    size_t GetAdjacentEdgesCount(Vertex<T> vertex) override {
+        return 0;
+    }
+
+private:
+    std::map<Vertex<T>, std::vector<Edge<T>>> adjacency_lists_;
+};
+
