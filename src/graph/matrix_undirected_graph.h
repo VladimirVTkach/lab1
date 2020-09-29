ac95f51681326519f1dae7e61d842c0ebef2a70c
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-30 00:44:12 +0300

    IsLinked fixed: wrong call syntax to contains method is corrected

--- src/graph/matrix_undirected_graph.h
+++ src/graph/matrix_undirected_graph.h
@@ -50,7 +50,7 @@ public:
     bool IsLinked() override {
         for (const auto &[k1, v1] : adjacency_matrix_) {
             for (const auto &[k2, v2]: adjacency_matrix_) {
-                if (!adjacency_matrix_[k1].contains[k2]) {
+                if (!adjacency_matrix_[k1].contains(k2)) {
                     return false;
                 }
             }
