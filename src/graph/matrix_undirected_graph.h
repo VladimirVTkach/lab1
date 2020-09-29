ac6121c1e4e14ff4b15a92960a873be4fe00d852
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-29 23:52:36 +0300

    MatrixUndirectedGraph class is declared

---
+++ src/graph/matrix_undirected_graph.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include "undirected_graph.h"
+
+template<typename T>
+class MatrixUndirectedGraph : public UndirectedGraph<T> {
+public:
+    void AddVertex(const T &vertex) override {
+
+    }
+
+    void AddEdge(const T &left, T &right) override {
+
+    }
+
+    void RemoveVertex(const T &vertex) override {
+
+    }
+
+    void RemoveEdge(const T &left, T &right) override {
+
+    }
+
+    bool IsLinked() override {
+        return false;
+    }
+
+    int GetDistance(const T &left, const T &right) override {
+        return 0;
+    }
+
+    size_t GetVerticesCount() override {
+        return 0;
+    }
+
+    size_t GetEdgesCount() override {
+        return 0;
+    }
+
+    size_t GetAdjacentEdgesCount(const T &vertex) override {
+        return 0;
+    }
+
+private:
+    std::vector<std::vector<Vertex<T>>> adjacency_matrix_;
+};
