2bfbdf185a0c737fcbccdb70c479b6e5126d96cf
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-08 19:02:54 +0300

    declaration comments are added

--- src/graph/vertex.h
+++ src/graph/vertex.h
@@ -1,36 +1,71 @@
 #pragma once
 
+/** Class representing graph vertex */
 template<typename T>
 class Vertex {
 public:
     Vertex() {}
 
+    /** Constructs Vertex instance with specified field's values
+     * @param data data to be stored in vertex
+     * */
     Vertex(T data) : data(data) {}
 
+    /** @return data stored in vertex*/
     T GetData() const {
         return data;
     }
 
+    /** Compares for equality this instance to other Vertex instance.
+    * If instances are equal returns true, otherwise false.
+    * @param rhs instance of Vertex to compare this instance to
+    * @return comparison result
+    * */
     bool operator==(const Vertex &rhs) const {
         return data == rhs.data;
     }
 
+    /** Compares for inequality this instance to other Vertex instance.
+     * If instances are not equal returns true, otherwise false.
+     * @param rhs instance of Vertex to compare this instance to
+     * @return comparison result
+     * */
     bool operator!=(const Vertex &rhs) const {
         return !(rhs == *this);
     }
 
+    /** Compares the relative order of this instance to other Vertex instance.
+     * If this instances is less then other instance returns true, otherwise false.
+     * @param rhs instance of Vertex to compare this instance to
+     * @return comparison result
+     * */
     bool operator<(const Vertex &rhs) const {
         return data < rhs.data;
     }
 
+    /** Compares the relative order of this instance to other Vertex instance.
+     * If this instances is greater then other instance returns true, otherwise false.
+     * @param rhs instance of Vertex to compare this instance to
+     * @return comparison result
+     * */
     bool operator>(const Vertex &rhs) const {
         return rhs < *this;
     }
 
+    /** Compares the relative order of this instance to other Vertex instance.
+     * If this instances is less than or equals to other instance returns true, otherwise false.
+     * @param rhs instance of Vertex to compare this instance to
+     * @return comparison result
+     * */
     bool operator<=(const Vertex &rhs) const {
         return !(rhs < *this);
     }
 
+    /** Compares the relative order of this instance to other Vertex instance.
+     * If this instances is greater than or equals to other instance returns true, otherwise false.
+     * @param rhs instance of Vertex to compare this instance to
+     * @return comparison result
+     * */
     bool operator>=(const Vertex &rhs) const {
         return !(*this < rhs);
     }
