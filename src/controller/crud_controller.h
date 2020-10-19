f4b124333aa0a34f968c290e234d0cae65e0bd49
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-19 19:39:42 +0300

    call to ShowMainMenu added to each method after main logic is executed

--- src/controller/crud_controller.h
+++ src/controller/crud_controller.h
@@ -14,12 +14,14 @@ public:
         T vertex = console_view_.GetUserInput<T>("Enter vertex value");
         undirected_graph_.AddVertex(vertex);
         console_view_.ShowGraph(undirected_graph_);
+        console_view_.ShowMainMenu();
     }
 
     void RemoveVertex() {
         T vertex = console_view_.GetUserInput<T>("Enter vertex value");
         undirected_graph_.RemoveVertex(vertex);
         console_view_.ShowGraph(undirected_graph_);
+        console_view_.ShowMainMenu();
     }
 
     void AddEdge() {
@@ -27,6 +29,7 @@ public:
         T right_vertex = console_view_.GetUserInput<T>("Enter right vertex value");
         undirected_graph_.AddEdge(left_vertex, right_vertex);
         console_view_.ShowGraph(undirected_graph_);
+        console_view_.ShowMainMenu();
     }
 
     void RemoveEdge() {
@@ -34,26 +37,31 @@ public:
         T right_vertex = console_view_.GetUserInput<T>("Enter right vertex value");
         undirected_graph_.RemoveEdge(left_vertex, right_vertex);
         console_view_.ShowGraph(undirected_graph_);
+        console_view_.ShowMainMenu();
     }
 
     void GetVerticesCount() {
         size_t vertices_count = undirected_graph_.GetVerticesCount();
         console_view_.ShowMessage(std::to_string(vertices_count));
+        console_view_.ShowMainMenu();
     }
 
     void GetEdgesCount() {
         size_t edges_count = undirected_graph_.GetEdgesCount();
         console_view_.ShowMessage(std::to_string(edges_count));
+        console_view_.ShowMainMenu();
     }
 
     void GetAdjacentEdgesCount() {
         T vertex = console_view_.GetUserInput<T>("Enter vertex value");
         size_t adjacent_edges_count = undirected_graph_.GetAdjacentEdgesCount(vertex);
         console_view_.ShowMessage(std::to_string(adjacent_edges_count));
+        console_view_.ShowMainMenu();
     }
 
     void Clear() {
         undirected_graph_.Clear();
+        console_view_.ShowMainMenu();
     }
 
 private:
