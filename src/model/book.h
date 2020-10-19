4b2266e82f53fbf0b0983d332c43884dc485aefb
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-19 23:22:59 +0300

    comments are moved to th file with Book class implementation

--- src/model/book.h
+++ src/model/book.h
@@ -15,14 +15,6 @@ class Book {
 public:
     Book();
 
-    /** Constructs Book instance with specified field's values
-     * @param name book's name
-     * @param authors list of book authors
-     * @param pages count of pages in book
-     * @param release_date book's release date
-     * @param description book's short description
-     * @param characters book's characters each stored with key corresponding to their roles
-     * */
     Book(const std::string &name,
          const std::vector<Author> &authors,
          int pages,
@@ -30,22 +22,16 @@ public:
          const std::string &description,
          const std::map<Character *, CharacterRole> &characters);
 
-    /** @return book's name */
     const std::string &GetName() const;
 
-    /** @return list of book authors */
     const std::vector<Author> &GetAuthors() const;
 
-    /** @return count of pages in book */
     int GetPages() const;
 
-    /** @return book's release date */
     const std::chrono::year_month_day &GetReleaseDate() const;
 
-    /** @return book's short description */
     const std::string &GetDescription() const;
 
-    /** @return book's characters each stored with key corresponding to their roles */
     const std::map<Character *, CharacterRole> &GetCharacters() const;
 
     bool operator==(const Book &rhs) const;
