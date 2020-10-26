df1eaaea06956a77c8bc15b0333e09fa16f1d2df
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-26 16:58:49 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include "../tst/tests_runner.h"
+#include "graph/adjacent_undirected_graph.h"
+#include "graph/matrix_undirected_graph.h"
+#include "view/console_view.h"
+#include "controller/main_menu_controller.h"
 
 int main() {
-    return run_all_tests();
+// Runs all tests
+//    run_all_tests();
+
+    AdjacentUndirectedGraph<std::string> graph;
+    ConsoleView console_view;
+    MainMenuController<std::string> main_menu_controller(graph, console_view);
+    main_menu_controller.Start();
 }
