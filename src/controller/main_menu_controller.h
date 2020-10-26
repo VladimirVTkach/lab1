2458cb9bf602178a86a1d0f0d1520d3b07ed65a0
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-27 00:40:22 +0200

    crud_controller_ field is set by constructor arg

--- src/controller/main_menu_controller.h
+++ src/controller/main_menu_controller.h
@@ -8,34 +8,34 @@ template<typename T>
 class MainMenuController {
 public:
     MainMenuController(UndirectedGraph<T> &undirected_graph,
-                       const ConsoleView &console_view) : undirected_graph_(undirected_graph),
-                                                          console_view_(console_view),
-                                                          crud_controller(CrudController<T>(undirected_graph,
-                                                                                            console_view)) {}
+                       const ConsoleView &console_view,
+                       CrudController<T> &crud_controller) : undirected_graph_(undirected_graph),
+                                                             console_view_(console_view),
+                                                             crud_controller_(crud_controller) {}
 
     void Start() {
         console_view_.ShowMainMenu();
         while (true) {
-            int option = console_view_.GetUserInput<int>("Your option");
+            int option = std::stoi(console_view_.GetUserInput("Your option"));
             if (option <= 0 || option > 10) {
                 console_view_.ShowMessage("Invalid option chosen");
             } else {
                 if (option == 1) {
-                    crud_controller.AddVertex();
+                    crud_controller_.AddVertex();
                 } else if (option == 2) {
-                    crud_controller.RemoveVertex();
+                    crud_controller_.RemoveVertex();
                 } else if (option == 3) {
-                    crud_controller.AddEdge();
+                    crud_controller_.AddEdge();
                 } else if (option == 4) {
-                    crud_controller.RemoveEdge();
+                    crud_controller_.RemoveEdge();
                 } else if (option == 5) {
-                    crud_controller.GetVerticesCount();
+                    crud_controller_.GetVerticesCount();
                 } else if (option == 6) {
-                    crud_controller.GetEdgesCount();
+                    crud_controller_.GetEdgesCount();
                 } else if (option == 7) {
-                    crud_controller.GetAdjacentEdgesCount();
+                    crud_controller_.GetAdjacentEdgesCount();
                 } else if (option == 8) {
-                    crud_controller.Clear();
+                    crud_controller_.Clear();
                 } else if (option == 9) {
                     console_view_.ShowGraph(undirected_graph_);
                 } else if (option == 10) {
@@ -48,5 +48,5 @@ public:
 private:
     UndirectedGraph<T> &undirected_graph_;
     ConsoleView console_view_;
-    CrudController<T> crud_controller;
+    CrudController<T> &crud_controller_;
 };
