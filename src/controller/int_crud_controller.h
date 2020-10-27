f59096be2bb987f13779864b2fc90872a47260f6
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-10-27 11:00:43 +0200

    GetVertex have been implemented

--- src/controller/int_crud_controller.h
+++ src/controller/int_crud_controller.h
@@ -6,20 +6,6 @@ class IntCrudController : public CrudController<int> {
 public:
     IntCrudController(UndirectedGraph<int> &undirected_graph,
                       const ConsoleView &console_view);
-
-    void AddVertex() override;
-
-    void RemoveVertex() override;
-
-    void AddEdge() override;
-
-    void RemoveEdge() override;
-
-    void GetVerticesCount() override;
-
-    void GetEdgesCount() override;
-
-    void GetAdjacentEdgesCount() override;
-
-    void Clear() override;
+protected:
+    int GetVertex(std::string message) override;
 };
