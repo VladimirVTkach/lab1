0cea97bcef2d01bb3655354777ef727939e9d6a7
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-02 21:49:31 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,21 @@
 #include "graph/adjacent_undirected_graph.h"
 #include "graph/matrix_undirected_graph.h"
 #include "view/console_view.h"
+#include "controller/int_crud_controller.h"
+#include "controller/double_crud_controller.h"
+#include "controller/string_crud_controller.h"
+#include "controller/vector_crud_controller.h"
 #include "controller/main_menu_controller.h"
 
 int main() {
 // Runs all tests
 //    run_all_tests();
 
-    AdjacentUndirectedGraph<std::string> graph;
+    AdjacentUndirectedGraph<std::vector<int>> graph;
     ConsoleView console_view;
-    MainMenuController<std::string> main_menu_controller(graph, console_view);
+    VectorCrudController vectorCrudController(graph, console_view);
+    MainMenuController<std::vector<int>> main_menu_controller(graph,
+                                                    console_view,
+                                                    vectorCrudController);
     main_menu_controller.Start();
 }
