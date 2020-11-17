bcd6e70b0f5bd19fc7d88e946d5f48c28bb94762
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-17 11:24:01 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,18 @@
 #include "controller/double_crud_controller.h"
 #include "controller/string_crud_controller.h"
 #include "controller/vector_crud_controller.h"
+#include "controller/book_crud_controller.h"
 #include "controller/main_menu_controller.h"
 
 int main() {
 // Runs all tests
-//    run_all_tests();
+    run_all_tests();
 
-    AdjacentUndirectedGraph<std::vector<int>> graph;
-    ConsoleView console_view;
-    VectorCrudController vectorCrudController(graph, console_view);
-    MainMenuController<std::vector<int>> main_menu_controller(graph,
-                                                    console_view,
-                                                    vectorCrudController);
-    main_menu_controller.Start();
+//    AdjacentUndirectedGraph<int> graph;
+//    ConsoleView console_view;
+//    IntCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<int> main_menu_controller(graph,
+//                                                    console_view,
+//                                                    book_crud_controller);
+//    main_menu_controller.Start();
 }
