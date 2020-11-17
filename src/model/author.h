f847a3d899d7e347eb12fecebe42d6c0d4862ed7
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-17 09:12:47 +0200

    additional comments added

--- src/model/author.h
+++ src/model/author.h
@@ -9,32 +9,39 @@ class Author {
 public:
     Author();
 
+    /** Constructs Author instance with specified field's values */
     Author(const std::string &first_name,
            const std::string &last_name,
            const std::chrono::year_month_day &date_of_birth);
 
-    /** @return author's first name */
+    /** Returns author first name */
     const std::string &GetFirstName() const;
 
-    /** @return author's last name */
+    /** Returns author last name */
     const std::string &GetLastName() const;
 
-    /** @return author's date of birth */
+    /** Returns author date of birth */
     const std::chrono::year_month_day &GetDateOfBirth() const;
 
-
+    /** Compares two authors for equality */
     bool operator==(const Author &rhs) const;
 
+    /** Compares two authors for inequality */
     bool operator!=(const Author &rhs) const;
 
+    /** Check whether current author is less then other */
     bool operator<(const Author &rhs) const;
 
+    /** Check whether current author is greater then other */
     bool operator>(const Author &rhs) const;
 
+    /** Check whether current author is less than or equal to other */
     bool operator<=(const Author &rhs) const;
 
+    /** Check whether current author is greater than or equal to other */
     bool operator>=(const Author &rhs) const;
 
+    /** Output author to the target stream */
     friend std::ostream &operator<<(std::ostream &os, const Author &author);
 
 private:
