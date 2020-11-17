5b4db119bedec6d18a589062ddd6d13fd37a8afc
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-17 09:20:22 +0200

    additional comments added

--- src/model/character.h
+++ src/model/character.h
@@ -15,29 +15,41 @@ class Character {
 public:
     Character();
 
+    /** Constructs Character instance with specified field's values */
     Character(const std::string &name,
               const std::string &biography);
 
+    /** Returns character name */
     const std::string &GetName() const;
 
+    /** Returns character biography */
     const std::string &GetBiography() const;
 
+    /** Adds mention about character and his role in some book */
     void AddMention(Book *book, CharacterRole characterRole);
 
+    /** Returns all mentions about character and his roles in different books */
     const std::map<Book *, CharacterRole> &GetMentions() const;
 
+    /** Compares two characters for equality */
     bool operator==(const Character &rhs) const;
 
+    /** Compares two characters for inequality */
     bool operator!=(const Character &rhs) const;
 
+    /** Check whether current character is less then other */
     bool operator<(const Character &rhs) const;
 
+    /** Check whether current character is greater then other */
     bool operator>(const Character &rhs) const;
 
+    /** Check whether current character is less than or equal to other */
     bool operator<=(const Character &rhs) const;
 
+    /** Check whether current character is greater than or equal to other */
     bool operator>=(const Character &rhs) const;
 
+    /** Output character to the target stream */
     friend std::ostream &operator<<(std::ostream &os, const Character &character);
 
 private:
