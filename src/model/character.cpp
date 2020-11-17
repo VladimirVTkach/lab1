5bac0bca6222322b5502865ce85f23addd12d1a5
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-17 09:27:57 +0200

    additional comments added

--- src/model/character.cpp
+++ src/model/character.cpp
@@ -22,24 +22,37 @@ const std::string &Character::GetBiography() const {
     return Character::biography_;
 }
 
-void Character::AddMention(Book *book, CharacterRole characterRole) {
-    mentions_[book] = characterRole;
+/** Adds mention about character and his role in some book
+ *  @param book book to add mention for
+ *  @param character_role character role in specified book
+ *  */
+void Character::AddMention(Book *book, CharacterRole character_role) {
+    mentions_[book] = character_role;
 }
 
+/** Returns all mentions about character and his roles in different books
+ * @return map of Book* to Character role representing character mentions
+ * */
 const std::map<Book *, CharacterRole> &Character::GetMentions() const {
     return mentions_;
 }
 
+/** @param rhs character to compare with
+ *  @return true if current character equals to rhs */
 bool Character::operator==(const Character &rhs) const {
     return name_ == rhs.name_ &&
            biography_ == rhs.biography_ &&
            mentions_ == rhs.mentions_;
 }
 
+/** @param rhs character to compare with
+ *  @return true if current character not equals to rhs */
 bool Character::operator!=(const Character &rhs) const {
     return !(rhs == *this);
 }
 
+/** @param rhs character to compare with
+ *  @return true if current book is less than rhs */
 bool Character::operator<(const Character &rhs) const {
     if (name_ < rhs.name_)
         return true;
@@ -52,9 +65,12 @@ bool Character::operator<(const Character &rhs) const {
     return mentions_ < rhs.mentions_;
 }
 
-std::ostream &operator<<(std::ostream &os, const std::map<Book *, CharacterRole> &mentions_) {
+/** @param os target output stream
+ *  @param mentions map of mentions for output
+ * */
+std::ostream &operator<<(std::ostream &os, const std::map<Book *, CharacterRole> &mentions) {
     os << "{";
-    for (const auto &[key, value]: mentions_) {
+    for (const auto &[key, value]: mentions) {
         os << "{" << key << ": " << value << "}";
         os << ",";
     }
@@ -62,20 +78,29 @@ std::ostream &operator<<(std::ostream &os, const std::map<Book *, CharacterRole>
     return os;
 }
 
+/** @param os target output stream
+ *  @param character character for output
+ * */
 std::ostream &operator<<(std::ostream &os, const Character &character) {
     os << "name_: " << character.name_ << " biography_: " << character.biography_ << " mentions_: "
        << character.mentions_;
     return os;
 }
 
+/** @param rhs character to compare with
+ *  @return true if current book is greater than rhs */
 bool Character::operator>(const Character &rhs) const {
     return rhs < *this;
 }
 
+/** @param rhs character to compare with
+ *  @return true if current book is less than or equals to rhs */
 bool Character::operator<=(const Character &rhs) const {
     return !(rhs < *this);
 }
 
+/** @param rhs character to compare with
+ *  @return true if current book is greater than or equals to rhs */
 bool Character::operator>=(const Character &rhs) const {
     return !(*this < rhs);
 }
