e29e3592076b63c1811ec7849061fb8faaf64d1e
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-11-24 02:24:20 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../tst/tests_runner.h"
 #include "graph/adjacent_undirected_graph.h"
+#include "graph/mapping_undirected_graph.h"
 #include "graph/matrix_undirected_graph.h"
 #include "view/console_view.h"
 #include "controller/int_crud_controller.h"
@@ -11,7 +12,7 @@
 
 int main() {
 // Runs all tests
-    run_all_tests();
+//    run_all_tests();
 
 //    AdjacentUndirectedGraph<int> graph;
 //    ConsoleView console_view;
@@ -20,7 +21,15 @@ int main() {
 //                                                    console_view,
 //                                                    book_crud_controller);
 //    main_menu_controller.Start();
-
+//
+//    MappingUndirectedGraph<int> graph;
+//    ConsoleView console_view;
+//    IntCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<int> main_menu_controller(graph,
+//                                                 console_view,
+//                                                 book_crud_controller);
+//    main_menu_controller.Start();
+//
 //    AdjacentUndirectedGraph<double> graph;
 //    ConsoleView console_view;
 //    DoubleCrudController book_crud_controller(graph, console_view);
@@ -28,7 +37,15 @@ int main() {
 //                                                 console_view,
 //                                                 book_crud_controller);
 //    main_menu_controller.Start();
-
+//
+//    MappingUndirectedGraph<double> graph;
+//    ConsoleView console_view;
+//    DoubleCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<double> main_menu_controller(graph,
+//                                                 console_view,
+//                                                 book_crud_controller);
+//    main_menu_controller.Start();
+//
 //    AdjacentUndirectedGraph<std::string> graph;
 //    ConsoleView console_view;
 //    StringCrudController book_crud_controller(graph, console_view);
@@ -36,7 +53,15 @@ int main() {
 //                                                 console_view,
 //                                                 book_crud_controller);
 //    main_menu_controller.Start();
-
+//
+//    MappingUndirectedGraph<std::string> graph;
+//    ConsoleView console_view;
+//    StringCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<std::string> main_menu_controller(graph,
+//                                                 console_view,
+//                                                 book_crud_controller);
+//    main_menu_controller.Start();
+//
 //    AdjacentUndirectedGraph<std::vector<int>> graph;
 //    ConsoleView console_view;
 //    VectorCrudController book_crud_controller(graph, console_view);
@@ -44,8 +69,24 @@ int main() {
 //                                                  console_view,
 //                                                  book_crud_controller);
 //    main_menu_controller.Start();
-
+//
+//    MappingUndirectedGraph<std::vector<int>> graph;
+//    ConsoleView console_view;
+//    VectorCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<std::vector<int>> main_menu_controller(graph,
+//                                                  console_view,
+//                                                  book_crud_controller);
+//    main_menu_controller.Start();
+//
 //    AdjacentUndirectedGraph<Book> graph;
+//    ConsoleView console_view;
+//    BookCrudController book_crud_controller(graph, console_view);
+//    MainMenuController<Book> main_menu_controller(graph,
+//                                                    console_view,
+//                                                    book_crud_controller);
+//    main_menu_controller.Start();
+//
+//    MappingUndirectedGraph<Book> graph;
 //    ConsoleView console_view;
 //    BookCrudController book_crud_controller(graph, console_view);
 //    MainMenuController<Book> main_menu_controller(graph,
